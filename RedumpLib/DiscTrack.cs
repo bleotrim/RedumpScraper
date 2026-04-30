fc6595d213924fb1b7e54e47919b2accba84caa9
Leotrim <bleotrim@gmail.com>
2026-04-30 22:02:08 +0200

    Fix possibly null reference warnings

--- RedumpLib/DiscTrack.cs
+++ RedumpLib/DiscTrack.cs
@@ -1,16 +1,16 @@
 public class DiscTrack
 {
-    public string Number { get; set; }
-    public string Type { get; set; }
-    public string Pregap { get; set; }
-    public string Length { get; set; }
-    public string Sectors { get; set; }
-    public string Size { get; set; }
-    public string Crc32 { get; set; }
-    public string Md5 { get; set; }
-    public string Sha1 { get; set; }
+    public string? Number { get; set; }
+    public string? Type { get; set; }
+    public string? Pregap { get; set; }
+    public string? Length { get; set; }
+    public string? Sectors { get; set; }
+    public string? Size { get; set; }
+    public string? Crc32 { get; set; }
+    public string? Md5 { get; set; }
+    public string? Sha1 { get; set; }
 
-    public DiscTrack(string Number, string Type, string Pregap, string Length, string Sectors, string Size, string Crc32, string Md5, string Sha1)
+    public DiscTrack(string? Number, string? Type, string? Pregap, string? Length, string? Sectors, string? Size, string? Crc32, string? Md5, string? Sha1)
     {
         this.Number = Number;
         this.Type = Type;
