cc8c3678e6187aaa636e33ee2c4076bbdd0263a4
Leotrim <bleotrim@gmail.com>
2026-04-30 16:42:09 +0200

    Add MongoDB Playground for query purposes

---
+++ RedumpDatabase/playground-2.mongodb.js
@@ -0,0 +1,33 @@
+/* global use, db */
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use('redump');
+
+// Search for documents in the current collection.
+db.getCollection('discs')
+  .find(
+    {
+      // Corretto: $ne invece di $neq
+      "title": { $ne: 'N/D' },
+      //"libcrypt_sectors": { $eq: null },
+      //"pvd_entries": [],
+      //"disc_id": '2'
+      //"rings": null
+      "game_info.languages": []
+    },
+    {
+      /*
+      * Projection
+      * _id: 0, // exclude _id
+      * fieldA: 1 // include field
+      */
+    }
+  )
+  .sort({
+    /*
+    * fieldA: 1 // ascending
+    * fieldB: -1 // descending
+    */
+  });
