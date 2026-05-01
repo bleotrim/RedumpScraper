1fa86ea0006d6e1cb73bba7d5ce02d15a3808c9b
Leotrim <bleotrim@gmail.com>
2026-05-01 09:22:12 +0200

    Fix warnigns for test id-47415

--- RedumpLib.Tests/ID47415HeaderTests.cs
+++ RedumpLib.Tests/ID47415HeaderTests.cs
@@ -24,6 +24,7 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_HasCorrectNumberOfEntries()
     {
         // Assert - Should have 16 header entries (0x00 to 0xF0)
+        Assert.NotNull(_disc.HeaderEntries);
         Assert.Equal(16, _disc.HeaderEntries.Count);
     }
 
@@ -31,6 +32,7 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_FirstRowContainsSegaSegakatana()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var firstEntry = _disc.HeaderEntries[0];
         Assert.Equal("0000", firstEntry.Row);
         Assert.Equal("53 45 47 41 20 53 45 47 41 4B 41 54 41 4E 41 20", firstEntry.Contents);
@@ -41,6 +43,7 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_SecondRowContainsSegaEnterprises()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var secondEntry = _disc.HeaderEntries[1];
         Assert.Equal("0010", secondEntry.Row);
         Assert.Equal("53 45 47 41 20 45 4E 54 45 52 50 52 49 53 45 53", secondEntry.Contents);
@@ -51,6 +54,7 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_ThirdRowContainsBuildInfo()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var thirdEntry = _disc.HeaderEntries[2];
         Assert.Equal("0020", thirdEntry.Row);
         Assert.Equal("31 38 45 39 20 47 44 2D 52 4F 4D 31 2F 31 20 20", thirdEntry.Contents);
@@ -61,9 +65,11 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_FourthRowContainsRegion()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var fourthEntry = _disc.HeaderEntries[3];
         Assert.Equal("0030", fourthEntry.Row);
         Assert.Equal("20 55 20 20 20 20 20 20 32 37 39 39 41 31 30 20", fourthEntry.Contents);
+        Assert.NotNull(fourthEntry.Ascii);
         Assert.Contains("U", fourthEntry.Ascii); // USA region
     }
 
@@ -71,9 +77,11 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_FifthRowContainsSerial()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var fifthEntry = _disc.HeaderEntries[4];
         Assert.Equal("0040", fifthEntry.Row);
         Assert.Equal("4D 4B 2D 35 31 30 35 37 20 20 56 31 2E 30 30 30", fifthEntry.Contents);
+        Assert.NotNull(fifthEntry.Ascii);
         Assert.Contains("MK-51057", fifthEntry.Ascii); // Disc serial
     }
 
@@ -81,6 +89,7 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_SixthRowContainsBuildDate()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var sixthEntry = _disc.HeaderEntries[5];
         Assert.Equal("0050", sixthEntry.Row);
         Assert.Equal("32 30 30 30 30 34 31 32 20 20 20 20 20 20 20 20", sixthEntry.Contents);
@@ -91,9 +100,11 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_SeventhRowContains1stReadBin()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var seventhEntry = _disc.HeaderEntries[6];
         Assert.Equal("0060", seventhEntry.Row);
         Assert.Equal("31 53 54 5F 52 45 41 44 2E 42 49 4E 20 20 20 20", seventhEntry.Contents);
+        Assert.NotNull(seventhEntry.Ascii);
         Assert.Contains("1ST_READ.BIN", seventhEntry.Ascii);
     }
 
@@ -101,6 +112,7 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_EighthRowContainsPublisher()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var eighthEntry = _disc.HeaderEntries[7];
         Assert.Equal("0070", eighthEntry.Row);
         Assert.Equal("53 45 47 41 20 45 4E 54 45 52 50 52 49 53 45 53", eighthEntry.Contents);
@@ -111,9 +123,11 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_NinthRowContainsProductTitle()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var ninthEntry = _disc.HeaderEntries[8];
         Assert.Equal("0080", ninthEntry.Row);
         Assert.Equal("47 45 4E 45 52 41 54 4F 52 20 32 20 20 20 20 20", ninthEntry.Contents);
+        Assert.NotNull(ninthEntry.Ascii);
         Assert.Contains("GENERATOR 2", ninthEntry.Ascii);
     }
 
@@ -121,25 +135,28 @@ public class ID47415HeaderTests : IClassFixture<ID47415Fixture>
     public void ParseHeader_LastRow()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         var lastEntry = _disc.HeaderEntries[15];
         Assert.Equal("00F0", lastEntry.Row);
         // The last row should contain spaces
-        Assert.NotEmpty(lastEntry.Contents);
-        Assert.NotEmpty(lastEntry.Ascii);
+        Assert.NotEmpty(lastEntry.Contents ?? "");
+        Assert.NotEmpty(lastEntry.Ascii ?? "");
     }
 
     [Fact]
     public void ParseHeader_AllRowsHaveHexFormat()
     {
         // Assert
+        Assert.NotNull(_disc.HeaderEntries);
         foreach (var entry in _disc.HeaderEntries)
         {
             // Row should be hex format (0000, 0010, etc.)
+            Assert.NotNull(entry.Row);
             Assert.Matches(@"^[0-9A-Fa-f]{4}$", entry.Row);
             // Contents should contain hex bytes
-            Assert.NotEmpty(entry.Contents);
+            Assert.NotEmpty(entry.Contents ?? "");
             // ASCII should not be empty
-            Assert.NotEmpty(entry.Ascii);
+            Assert.NotEmpty(entry.Ascii ?? "");
         }
     }
 }
